e21d31f5c15219a3a0ff1ad2258aa91c1beb9e1c
esslam emad <esslam69@gmail.com>
2018-12-31 16:44:29 +0200

    Initial Commit

---
+++ MinistryOfElectricity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinistryOfElectricity
+//
+//  Created by Esslam Emad on 31/12/18.
+//  Copyright © 2018 Alyom Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
